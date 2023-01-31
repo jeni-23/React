206edc4fcd97f7013294fa2dabcddf96b817d284
JeniAro <88921564+JeniAro@users.noreply.github.com>
2023-01-31 18:20:09 +0530

    counter-application

---
+++ src/Counter/counter.js
@@ -0,0 +1,32 @@
+import {React,useState} from "react";
+
+const Counter =()=>{
+	const [value,setValue]=useState(0);
+	 const increase = () => {
+    setValue(counter => counter + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setValue(counter => counter - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setValue(0)
+  }
+
+	return(
+<>
+<div>
+	<h1> Counter</h1>
+<h5>{value}</h5>
+	<button type="button" class="btn btn-info" id='add' onClick={increase}>Add</button>
+	<button type="button" class="btn btn-warning" id='add' onClick={decrease}>Sub</button>
+	<button type="button" class="btn btn-danger" id='add' onClick={reset}>Reset</button>
+</div>
+</>
+
+		)
+}
+export default Counter
